bb5f3bdf18e28c5ef8a037e50adee0a31adba422
Tilda L Aderud <tilda6633@gmail.com>
2024-09-30 21:08:11 +0200

    10 decimals

--- test.py
+++ test.py
@@ -1,54 +1,60 @@
 import math
 from decimal import Decimal, getcontext
 
-getcontext().prec = 25
 
-x0 = Decimal(0)
-y0 = Decimal(1)
-u0 = Decimal(0)
-h = Decimal(0.001)
+getcontext().prec = 50  # How many decimals we want
+
+
+x = Decimal(0)
+y = Decimal(1)
+u = Decimal(0)
+h = Decimal(0.01)
 tol = Decimal(1e-20)
 
-xPrev = x0
-yPrev = y0
-uPrev = u0
+xPrev = x
+yPrev = y
+uPrev = u
 
-# Definiera funktionen som representerar systemet y'' = -y
+
+# Represents the system y'' = -y
 def dudx6(x, y, u):
-    dydx = u    # u = y'
+    dydx = u    # y' = u
     dudx = -y   # y'' = -y
     return dydx, dudx
 
-while (True):
-    x = xPrev
-    y = yPrev
-    u = uPrev
-    while(True):
-        # Steg i Runge-Kutta algoritmen
-        k1 = dudx6(x, y, u)
-        k2 = dudx6(x + h/2, y + h*k1[0]/2, u + h*k1[1]/2)
-        k3 = dudx6(x + h/2, y + h*k2[0]/2, u + h*k2[1]/2)
-        k4 = dudx6(x + h, y + h*k3[0], u + h*k3[1])
 
-        xPrev = x
-        yPrev = y
-        uPrev = u
+while True:
+    # Runge-Kutta algorithm
+    k1 = dudx6(x, y, u)
+    k2 = dudx6(x + h / 2, y + h * k1[0] / 2, u + h * k1[1] / 2)
+    k3 = dudx6(x + h / 2, y + h * k2[0] / 2, u + h * k2[1] / 2)
+    k4 = dudx6(x + h, y + h * k3[0], u + h * k3[1])
 
-        # Uppdatera x, y och u
-        x = x + h
-        y = y + h*(k1[0] + 2*k2[0] + 2*k3[0] + k4[0]) / 6
-        u = u + h*(k1[1] + 2*k2[1] + 2*k3[1] + k4[1]) / 6
+    # Saving previous x, y and u
+    xPrev = x
+    yPrev = y
+    uPrev = u
 
-        # Kontrollera när y passerar 0 för att få π
-        if y < 0:
-            print(2*x)
-            if abs(2*x - Decimal(math.pi)) < tol:
-                print(2*x)
-                break
-            else:
-                h = h/2
-                break
+    # Updating x, y and u
+    x = x + h
+    y = y + h * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]) / 6
+    u = u + h * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]) / 6
+
+    # Checking when y passes 0
+    if y < 0:
+        approxPi = 2*x
+
+        if abs(approxPi - Decimal(math.pi)) < tol:
+            print("Converged result: ", str(approxPi) + " ± " + str(abs(approxPi - Decimal(math.pi))))
+            break
+        
+        # Returning to previous x, y and u values and making our steps 1/2 as big
+        x = xPrev 
+        y = yPrev
+        u = uPrev
+        h = h / 2
+        continue
 
 '''
-3.14159265358979323846
+3.141592653589793115997963
 '''
